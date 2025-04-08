2ce04a7c5d9f38b76e5ea78b85a4e3e8c222d7ef
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-08 23:56:17 +0200

    General Problem Solving By C++

---
+++ A. Easy Problem.cpp
@@ -0,0 +1,24 @@
+// A. Easy Problem
+#include <iostream>
+#include <string >
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+    int t  ;              cin >> t ;
+    int a ;          // (a , b) ;
+    while(t--)
+    {
+        cin >> a ;
+       cout << a - 1 << endl;
+
+    }
+    
+
+    return 0;
+}
