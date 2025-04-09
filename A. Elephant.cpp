2e5c7ab84885749bbb95cc42e00d09ecacbf457e
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-09 23:25:23 +0200

    General Problem Solving By C++

---
+++ A. Elephant.cpp
@@ -0,0 +1,13 @@
+// A. Elephant
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main()
+{
+    long long num ;              cin >> num ;
+    cout<< (num + 4 ) / 5 << endl;
+  
+    return 0 ;
+}
