0124777656ad245f435430b24bce95f7eb69acb8
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-05-01 22:55:35 +0200

    General Problem Solving By C++

---
+++ A. Short Sort.cpp
@@ -0,0 +1,30 @@
+// A. Short Sort
+#include <iostream>
+using namespace std;
+int main()
+{
+ int t ;  // test cases
+ cin>>t ;
+
+
+
+while(t--)
+{
+ string result ;
+ cin>>result ;
+
+ if (result[0]=='a' |result[1]=='b' || result[2]=='c' )
+ {
+  cout<<"YES"<<endl;
+ }
+ else
+ {
+  cout<<"NO"<<endl;
+ }
+
+}
+
+    return 0;
+}
+
+
