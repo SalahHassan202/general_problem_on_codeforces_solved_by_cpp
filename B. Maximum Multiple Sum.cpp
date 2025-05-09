e91955610172bcf215620063c9b8149263205769
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-05-09 22:37:18 +0200

    General Problem Solving By C++

---
+++ B. Maximum Multiple Sum.cpp
@@ -0,0 +1,24 @@
+// B. Maximum Multiple Sum
+#include <iostream>
+#include <algorithm>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int testCases ;                cin >> testCases ;
+
+    while(testCases--)
+    {
+        int num ;                  cin >> num ; 
+
+        int x = (num == 3  ?  3 : 2) ;
+
+        cout << x << endl;
+
+    }
+    
+    
+
+    return 0;
+}
