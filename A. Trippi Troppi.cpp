4c34ad22744f5b84ca8bf21e9b55da10025c1c01
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-09-22 23:07:19 +0300

    General Problem Solving By C++

---
+++ A. Trippi Troppi.cpp
@@ -0,0 +1,26 @@
+// A. Trippi Troppi
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std ;
+void solve()
+{
+    string s1 , s2 , s3 ; 
+    cin>> s1 >>s2 >> s3 ;
+
+    cout<< s1[0] <<s2[0] <<s3[0] <<endl;
+}
+int main()
+{
+    // fast code
+    ios::sync_with_stdio(false),cout.tie(NULL),cin.tie(0);
+    
+   ll t ; cin >>t ;
+   while(t--)
+   {
+        
+        solve();
+
+   }
+    return 0 ;
+}
