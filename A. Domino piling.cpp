c6b2bfec8c623a9086abb41f0c543563c2778d08
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-09-27 02:28:19 +0300

    General Problem Solving By C++

---
+++ A. Domino piling.cpp
@@ -0,0 +1,28 @@
+// A. Domino piling
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std ;
+void solve()
+{
+    ll m , n ;
+    cin >> m >> n ;
+    ll res = (m*n)/2 ; 
+    cout<< res<< endl;
+
+}
+int main()
+{
+    // fast code
+    ios::sync_with_stdio(false),cout.tie(NULL),cin.tie(0);
+    
+    ll t = 1 ;
+    // cin>> t ;
+    while(t--)
+    {
+        solve();
+    }
+  
+    
+    return 0 ;
+}
