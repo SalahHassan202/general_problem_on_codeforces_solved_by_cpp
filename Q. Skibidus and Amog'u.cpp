1c827950dd379de5ec6ace4ba6c3ccc62ca00487
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-09-29 20:52:44 +0300

    General Problem Solving By C++

---
+++ Q. Skibidus and Amog'u.cpp
@@ -0,0 +1,30 @@
+// Q. Skibidus and Amog'u
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std ;
+void solve()
+{
+    string s ; cin >> s ;
+    for(int i =0;i<s.size()-2 ; i++)
+    {
+        cout << s[i];
+    }
+    cout<< "i" << endl;
+
+}
+int main()
+{
+    // fast code
+    ios::sync_with_stdio(false),cout.tie(NULL),cin.tie(0);
+    
+    ll t = 1 ;
+    cin>> t ;
+    while(t--)
+    {
+        solve();
+    }
+  
+    
+    return 0 ;
+}
