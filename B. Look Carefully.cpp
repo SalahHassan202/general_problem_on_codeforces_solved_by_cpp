5aa1a98df8f211dad8ac0feee91b2b81428eecd2
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-10-01 01:55:06 +0300

    General Problem Solving By C++

---
+++ B. Look Carefully.cpp
@@ -0,0 +1,29 @@
+// B. Look Carefully
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std ;
+
+
+void solve()
+{
+    char a;
+    cin >> a; 
+    
+    if (a == 'C' || a == 'h' || a == 'i') 
+        cout << "Yes\n";
+     else 
+        cout << "No\n";
+    
+
+}
+int main()
+{
+    // fast code
+    ios::sync_with_stdio(false),cout.tie(NULL),cin.tie(0);
+ 
+    solve();
+   
+    
+    return 0 ;
+}
