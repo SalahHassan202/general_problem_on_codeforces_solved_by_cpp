7914333d8a3fdc9e114859e2abc46e1931eb02e4
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-10-01 01:52:44 +0300

    General Problem Solving By C++

---
+++ A. Race.cpp
@@ -0,0 +1,31 @@
+// A. Race
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std ;
+void solve()
+{
+  vector<ll>v(3);
+  for(int i = 0 ; i <3 ;i++)
+  {
+    cin>>v[i] ;
+  }
+  sort(v.begin() , v.end());
+
+  cout<<v[0] <<endl;
+}
+int main()
+{
+    // fast code
+    ios::sync_with_stdio(false),cout.tie(NULL),cin.tie(0);
+    
+    ll t = 1 ;
+    //cin>> t ;
+    while(t--)
+    {
+        solve();
+    }
+  
+    
+    return 0 ;
+}
