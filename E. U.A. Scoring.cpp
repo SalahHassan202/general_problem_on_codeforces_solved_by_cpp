af8f416d3cf85d8c0f07e1da16eecf0724e83d73
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-10-03 23:21:00 +0300

    General Problem Solving By C++

---
+++ E. U.A. Scoring.cpp
@@ -0,0 +1,27 @@
+// E. U.A. Scoring
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std ;
+
+void solve()
+{
+    ll n ; cin >> n ;
+    cout<<(n*(n+1)) << endl;
+   
+}
+int main()
+{
+    // fast code
+    ios::sync_with_stdio(false),cout.tie(NULL),cin.tie(0);
+    
+    ll t = 1 ;
+   //cin >> t ;
+    while(t--)
+    {
+        solve();
+    }
+  
+    
+    return 0 ;
+}
