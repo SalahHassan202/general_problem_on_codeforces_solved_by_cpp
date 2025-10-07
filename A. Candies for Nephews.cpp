e1589f293bafb62737165ea94bd009c9a59436b3
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-10-07 23:46:51 +0300

    General Problem Solving By C++

---
+++ A. Candies for Nephews.cpp
@@ -0,0 +1,31 @@
+// A. Candies for Nephews
+
+#include<bits/stdc++.h>
+#define ll long long
+#define fast ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using namespace std ;
+
+void solve()
+{
+    ll n  ;  cin >> n  ;
+    ll r = n % 3 ;
+    if( r == 0)
+    cout << 0 << endl;
+    else
+    cout << 3-r<< endl;
+    
+}
+int main()
+{
+    fast;
+    
+    ll t = 1 ;
+    // cin >> t ;
+    while(t--)
+    {
+        solve();
+    }
+  
+    
+    return 0 ;
+}
