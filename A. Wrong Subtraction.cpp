969c8b74a7d6d7c79de525b6a319445d1b8ff646
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-10-11 20:54:09 +0300

    General Problem Solving By C++

---
+++ A. Wrong Subtraction.cpp
@@ -0,0 +1,35 @@
+// A. Wrong Subtraction
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    while (k--)
+    {
+        if (n % 10 != 0)
+            n -= 1;
+        else
+            n /= 10;
+    }
+    cout << n << endl;
+}
+int main()
+{
+    fast;
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
