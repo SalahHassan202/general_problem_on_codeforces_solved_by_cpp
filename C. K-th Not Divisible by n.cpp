d861d54a672188ab149ac8acbd785c85f2c0e4f3
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-10-11 23:58:09 +0300

    General Problem Solving By C++

---
+++ C. K-th Not Divisible by n.cpp
@@ -0,0 +1,29 @@
+// C. K-th Not Divisible by n
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    ll ans = k + ((k - 1) / (n - 1));
+    cout << ans << endl;
+}
+int main()
+{
+    fast;
+
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
