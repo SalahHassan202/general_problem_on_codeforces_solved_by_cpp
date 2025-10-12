21e463ca480577ec36abcc84052b24c6dbf9294a
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-10-13 02:14:18 +0300

    General Problem Solving By C++

---
+++ A. Blackboard Game.cpp
@@ -0,0 +1,28 @@
+// A. Blackboard Game
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << (n % 4 ? "Alice" : "Bob") << '\n';
+}
+int main()
+{
+    fast;
+
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
