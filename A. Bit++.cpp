63394baa68fa15bc0bd90578f543709135453c45
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-10-17 23:32:39 +0300

    General Problem Solving By C++

---
+++ A. Bit++.cpp
@@ -0,0 +1,40 @@
+// A. Bit++
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll t;
+    cin >> t;
+    ll x = 0;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        if (s[1] == '+')
+            x++;
+        else
+            x--;
+    }
+
+    cout << x << endl;
+}
+int main()
+{
+    fast;
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
