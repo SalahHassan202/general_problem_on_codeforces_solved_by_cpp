144f87546e30a2e8fc36322c84cf590781040960
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-10-26 20:05:23 +0300

    General Problem Solving By C++

---
+++ B. Party.cpp
@@ -0,0 +1,34 @@
+// B. Party
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n, m, l, r;
+    cin >> n >> m >> l >> r;
+
+    if ((l * r) >= (n + m))
+        cout << "Yes";
+    else
+        cout << "No";
+}
+
+int main()
+{
+    fast;
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
