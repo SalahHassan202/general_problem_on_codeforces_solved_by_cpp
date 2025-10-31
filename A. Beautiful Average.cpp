6fecd5dd3ee18d97a3fff56d6f7efada65088692
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-10-31 22:26:33 +0200

    General Problem Solving By C++

---
+++ A. Beautiful Average.cpp
@@ -0,0 +1,36 @@
+// A. Beautiful Average
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+
+    ll n;
+    cin >> n;
+    vector<ll> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    cout << *max_element(v.begin(), v.end()) << endl;
+}
+
+int main()
+{
+    fast;
+
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
