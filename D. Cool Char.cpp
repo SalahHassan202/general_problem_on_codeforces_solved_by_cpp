3cb84901bd3648f603a8a6340373a7973e61a339
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-02 17:17:48 +0200

    General Problem Solving By C++

---
+++ D. Cool Char.cpp
@@ -0,0 +1,33 @@
+// D. Cool Char
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    char c;
+    cin >> c;
+    ll ans = c;
+    if (ans % 2 == 0)
+        cout << "even";
+    else
+        cout << "odd";
+}
+
+int main()
+{
+    fast;
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
