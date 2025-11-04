2185f42f92ff4da0b6b9b48add67902f93aef259
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-04 13:21:48 +0200

    Added B. Too Many Dresses! Problem

---
+++ B. Too Many Dresses!.cpp
@@ -0,0 +1,36 @@
+//  B. Too Many Dresses!
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    ll ans = 1;
+    for (int i = 0; i < m; i++)
+    {
+        ans *= (n - i);
+    }
+
+    cout << ans;
+}
+
+int main()
+{
+    fast;
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
