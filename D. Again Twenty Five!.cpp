3a6a7c47fdff529110a777e33f389730fc87dc9d
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-05 01:00:51 +0200

    Added D. Again Twenty Five! Problem

---
+++ D. Again Twenty Five!.cpp
@@ -0,0 +1,38 @@
+// D. Again Twenty Five!
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+
+    // n = 1 : 5ⁿ = 5 : 05
+    // n = 2 : 5ⁿ = 25 : 25
+    // n = 3 : 5ⁿ = 125 : 25
+    // n = 4 : 5ⁿ = 625 : 25
+    // n = 5 : 5ⁿ = 3125 : 25
+    // n = 6 : 5ⁿ = 15625 : 25
+
+    ll n;
+    cin >> n;
+    cout << 25;
+}
+
+int main()
+{
+    fast;
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
