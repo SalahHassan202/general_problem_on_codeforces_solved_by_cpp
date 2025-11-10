cddb44ccde5de7ebfd426aa4f426fbb93e907c79
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-10 23:33:41 +0200

    Added A. Among Us I Problem

---
+++ A. Among Us I.cpp
@@ -0,0 +1,23 @@
+// A. Among Us I
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n, x;
+    cin >> n >> x;
+    ll sum = n + x;
+    cout << sum;
+}
+
+int main()
+{
+    fast;
+    solve();
+    return 0;
+}
