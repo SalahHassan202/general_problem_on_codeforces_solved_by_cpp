2616ad0db55502c1b286fdd20ff962a974fb60d7
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-14 00:03:13 +0200

    Update A. Elephant.cpp

--- A. Elephant.cpp
+++ A. Elephant.cpp
@@ -1,13 +1,26 @@
 // A. Elephant
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <cmath>
-using namespace std;
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std ;
+void solve()
+{
+    ll n ;              cin >> n ;
+    cout<< (n + 4 ) / 5 << endl;
+
+}
 int main()
 {
-    long long num ;              cin >> num ;
-    cout<< (num + 4 ) / 5 << endl;
+    // fast code
+    ios::sync_with_stdio(false),cout.tie(NULL),cin.tie(0);
+    
+    ll t = 1 ;
+    // cin>> t ;
+    while(t--)
+    {
+        solve();
+    }
   
+    
     return 0 ;
 }
