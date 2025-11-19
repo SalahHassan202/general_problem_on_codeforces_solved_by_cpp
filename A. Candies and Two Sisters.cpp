03e18178a9fe05c3bbfd63b30402860500098575
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-19 19:56:00 +0200

    Added A. Candies and Two Sisters Problem

---
+++ A. Candies and Two Sisters.cpp
@@ -0,0 +1,28 @@
+// A. Candies and Two Sisters
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << (n - 1) / 2 << endl;
+}
+
+int main()
+{
+    fast;
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
