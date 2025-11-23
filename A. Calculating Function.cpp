5119f19de13d7190485d7d5a9a0a91f8cf840d8d
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-24 00:18:28 +0200

    Update A. Calculating Function.cpp

--- A. Calculating Function.cpp
+++ A. Calculating Function.cpp
@@ -1,29 +1,34 @@
 // A. Calculating Function
-#include<iostream>
-#include<string>
-#include<algorithm>
-using namespace std; 
-int main()
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
 {
-     /*
-     f(n) =  - 1 + 2 - 3 + .. + ( - 1)^ n * n .
-     f(4) = -1 + 2 -3 + 4 = 2 
-     output is 2 
-     f(4) = -1 + 2 -3 + 4 -5 = -3
-     output is -3 
-     ========================
-     ! Note that .......
- 
-     f(4) = 2 
-     num = 4 ,,,,  output = 2  ,,,,,       4 / 2  = 2 ==(output)
-     
-     f(5) = -3 
-     num = 5 ,,,,  output = -3  ,,,,,       5 / 2  = 2.5 (ignore furaction) == 2   (+1)  == 3    ==> *-1 ==(output) == -3
-     steps for the second solution 
-     num / 2 ,,,,  then + 1 for result  ,,,, then * -1 
-     
+    /*
+    f(n) =  - 1 + 2 - 3 + .. + ( - 1)^ n * n .
+    f(4) = -1 + 2 -3 + 4 = 2 
+    output is 2 
+    f(4) = -1 + 2 -3 + 4 -5 = -3
+    output is -3 
+    ========================
+    ! Note that .......
+
+    f(4) = 2 
+    num = 4 ,,,,  output = 2  ,,,,,       4 / 2  = 2 ==(output)
+    
+    f(5) = -3 
+    num = 5 ,,,,  output = -3  ,,,,,       5 / 2  = 2.5 (ignore furaction) == 2   (+1)  == 3    ==> *-1 ==(output) == -3
+    steps for the second solution 
+    num / 2 ,,,,  then + 1 for result  ,,,, then * -1 
+    
      */
-     long long num ;                cin >> num ;
+     ll num ;                cin >> num ;
  
      if(num % 2 == 0)        // even number 
      {
@@ -34,6 +39,17 @@ int main()
           cout<<-1*((num / 2) + 1)<<endl;
      }
  
-     
+
+   
+}
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
     return 0;
 }
