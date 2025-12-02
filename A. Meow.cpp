5f6d832067883f3edaa8372883631dfff6711a6c
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-12-02 22:55:58 +0200

    Added A. Meow Problem

---
+++ A. Meow.cpp
@@ -0,0 +1,30 @@
+
+
+// A. Meow
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    cout << s << "eow";
+}
+
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
