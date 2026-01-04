7fc43ab9b18326933038a6927de0625a227beec8
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-04 23:57:19 +0200

    Added A. ICPC Mansoura Loves U Problem

---
+++ A. ICPC Mansoura Loves U.cpp
@@ -0,0 +1,26 @@
+// A. ICPC Mansoura Loves U
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+using namespace std;
+
+void solve()
+{
+    string s;
+    getline(cin, s);
+    cout << s;
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
