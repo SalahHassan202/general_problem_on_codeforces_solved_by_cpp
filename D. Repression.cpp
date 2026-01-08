f4198c5c3420d1ffad0591383d62fe4f48429e0a
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-08 23:06:10 +0200

    Added D. Repression Problem

---
+++ D. Repression.cpp
@@ -0,0 +1,28 @@
+// D. Repression
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+using namespace std;
+
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    ll mx = max({a, b, c});
+    ll mn = min({a, b, c});
+    cout << a + b + c - mn;
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
