d98699f422a9a1c4f60a7ab7d49c4da77ac25a3a
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-08 22:45:45 +0200

    Added A. Hello World! Problem

---
+++ A. Hello World!.cpp
@@ -0,0 +1,24 @@
+// A. Hello World!
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+using namespace std;
+
+void solve()
+{
+    cout << "Hello World!";
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
