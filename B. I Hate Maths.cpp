961a5644a8618cdfa6c2717f074c1eb9165fd30b
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-08 22:48:34 +0200

    Added B. I Hate Maths Problem

---
+++ B. I Hate Maths.cpp
@@ -0,0 +1,26 @@
+// B. I Hate Maths
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    cout << a * b << endl;
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
