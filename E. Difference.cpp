8cf49994444389b6369e424738c1d8f3e2073ab5
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-08 23:07:23 +0200

    Added E. Difference Problem

---
+++ E. Difference.cpp
@@ -0,0 +1,26 @@
+// E. Difference
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+using namespace std;
+
+void solve()
+{
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << "DIFERENCA = " << a * b - c * d;
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
