53dcc76d647cac53ec8877b70af2045fa9d82bd1
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-10 16:15:40 +0200

    Added X. Easy Problem

---
+++ X. Easy Problem.cpp
@@ -0,0 +1,29 @@
+// X. Easy Problem
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n >= 13)
+        cout << "YES";
+    else
+        cout << "NO";
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
