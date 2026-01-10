8dce78c1a76d5bf2880ae17e42adbd293c46c990
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-10 21:18:18 +0200

    Added A. Welcome to our training Problem

---
+++ A. Welcome to our training.cpp
@@ -0,0 +1,26 @@
+// A. Welcome to our training
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+using namespace std;
+
+void solve()
+{
+    string s;
+    getline(cin, s);
+    cout << "YES";
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
