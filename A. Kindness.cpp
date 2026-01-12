1bd30d5544e773b4927933d076515e1d66b61ed9
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-12 16:29:20 +0200

    Added A. Kindness Problem

---
+++ A. Kindness.cpp
@@ -0,0 +1,25 @@
+// A. Kindness
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    cout << s.size();
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
