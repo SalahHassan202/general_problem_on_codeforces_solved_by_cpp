31f00e437d77bd5aa0b509c07309ae29e65d29e1
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-12 16:33:52 +0200

    Added C. Positions Problem

---
+++ C. Positions.cpp
@@ -0,0 +1,33 @@
+// C. Positions
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if (a[i] >= 10)
+        {
+            cout << "A[" << i << "] = " << a[i] << endl;
+        }
+    }
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
