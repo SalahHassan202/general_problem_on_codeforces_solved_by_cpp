a1275c81df47171ad0833fd12e32b1c82ffb3ec0
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-14 23:56:42 +0200

    Added I. Mirror Problem

---
+++ I. Mirror.cpp
@@ -0,0 +1,53 @@
+// I. Mirror
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a, a + n);
+
+    ll m = n / 2;
+
+    for (int i = 0; i < m; i++)
+    {
+        cout << a[i] << " ";
+    }
+    for (int i = m - 1; i >= 0; i--)
+    {
+        cout << a[i] << " ";
+    }
+
+    cout << endl;
+
+    for (int i = n - 1; i >= m; i--)
+    {
+        cout << a[i] << " ";
+    }
+    for (int i = m; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
