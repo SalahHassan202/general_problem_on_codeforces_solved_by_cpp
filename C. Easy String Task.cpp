06d84142d0a78459bab6def391ddd3edcc7a1c88
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-25 22:44:36 +0200

    Addded C. Easy String Task Problem

---
+++ C. Easy String Task.cpp
@@ -0,0 +1,26 @@
+// C. Easy String Task
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << s1.size() << " " << s2.size() << endl;
+
+    cout << s1 << " " << s2 << endl;
+}
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
