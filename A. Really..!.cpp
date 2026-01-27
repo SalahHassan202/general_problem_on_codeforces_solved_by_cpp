b30dc143cc878e30201c90998f890d46b113db34
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-01-27 23:07:58 +0200

    Added A. Really..! Problem

---
+++ A. Really..!.cpp
@@ -0,0 +1,28 @@
+// A. Really..!
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n % 2 == 0)
+        cout << "NO";
+    else
+        cout << "YES";
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
