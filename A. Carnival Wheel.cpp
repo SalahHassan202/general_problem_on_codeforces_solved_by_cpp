626be011e268570dcec88b691fd22444755e3f57
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-03-03 00:00:10 +0200

    Added A. Carnival Wheel Problem

---
+++ A. Carnival Wheel.cpp
@@ -0,0 +1,31 @@
+// A. Carnival Wheel
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll x, y, z;
+    cin >> x >> y >> z;
+    ll g = gcd(x, z);
+    cout << x - g + (y % g) << endl;
+}
+
+int main()
+{
+    fast;
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
