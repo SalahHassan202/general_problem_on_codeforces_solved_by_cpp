0ae5aac8f8f9608dff7dbead841c9d4be373af12
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-03-19 23:05:41 +0200

    Added B. Complicated GCD Problem

---
+++ B. Complicated GCD.cpp
@@ -0,0 +1,32 @@
+// B. Complicated GCD
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    string a, b;
+    cin >> a >> b;
+    if (a == b)
+    {
+        cout << a << endl;
+    }
+    else
+    {
+        cout << 1 << endl;
+    }
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
