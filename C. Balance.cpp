d2773a0d3b3dffd91c7a5e53275e09f802bf37d5
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-04-14 17:35:51 +0200

    Update C. Balance.cpp

--- C. Balance.cpp
+++ C. Balance.cpp
@@ -1,12 +1,11 @@
 // C. Balance
 
-
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std ;
 void solve()
 {
-  ll a , b ;  cin >> a >> b ;
+  ll a , b ;         cin >> a >> b ;
   if(a >b)
   {
     cout<<a-b <<" " <<"R"<<endl;
@@ -25,8 +24,7 @@ int main()
     // fast code
     ios::sync_with_stdio(false),cout.tie(NULL),cin.tie(0);
     
-    ll t = 1 ;
-    cin>> t ;
+    ll t = 1 ;            cin>> t ;
     while(t--)
     {
         solve();
