1133c537fd9de18f1ebc0d33e6c5bddd3f3d15ff
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-04-16 07:30:56 +0200

    Added A. Time Machine 2 Problem

---
+++ A. Time Machine 2.cpp
@@ -0,0 +1,26 @@
+// A. Time Machine 2
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    ll x = a ^ b;
+    cout << x << endl;
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
