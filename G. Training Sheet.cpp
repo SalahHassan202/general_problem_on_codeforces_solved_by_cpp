8cc5c2c1de773e2e7733163b106fba0505577560
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-04-16 07:32:24 +0200

    Added G. Training Sheet Problem

---
+++ G. Training Sheet.cpp
@@ -0,0 +1,25 @@
+// G. Training Sheet
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    cout << x * y << endl;
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
