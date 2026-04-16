766bbafb71203d7c0eacad17dcc9471b8b644dc7
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-04-16 07:31:14 +0200

    Added B. Factorial Problem

---
+++ B. Factorial.cpp
@@ -0,0 +1,32 @@
+// B. Factorial
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n >= 2)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
