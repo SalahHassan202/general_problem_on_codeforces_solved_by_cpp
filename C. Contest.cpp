a8534f7e178899c6caf13f517bf1352d587001b5
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-04-21 19:09:37 +0200

    Added C. Contest Problem

---
+++ C. Contest.cpp
@@ -0,0 +1,26 @@
+// C. Contest
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = (n + 3) * 4;
+    cout << ans << endl;
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
