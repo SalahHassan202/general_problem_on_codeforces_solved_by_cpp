3bd0efc1615786017eefeb918031e3bf897b402d
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-04-21 19:09:43 +0200

    Added C. Digit Problem

---
+++ C. Digit.cpp
@@ -0,0 +1,27 @@
+// C. Digit
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    char c;
+    cin >> c;
+    cout << c << endl;
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
