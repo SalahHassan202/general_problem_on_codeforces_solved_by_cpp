90b0d4b5a1b66d33f9572c59c9854bcf03975ffd
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-04-25 00:05:30 +0300

    Added D. Size Of The Box Problem

---
+++ D. Size Of The Box.cpp
@@ -0,0 +1,26 @@
+// D. Size Of The Box
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll x = (n / 100) % 10;
+    cout << x << endl;
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
