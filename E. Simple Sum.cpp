685b3e067d76e85de31b0fba42f9a6ae46a97dda
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-04-28 22:56:56 +0300

    Added E. Simple Sum Problem

---
+++ E. Simple Sum.cpp
@@ -0,0 +1,25 @@
+// E. Simple Sum
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+}
+
+int main()
+{
+    fast;
+
+    solve();
+
+    return 0;
+}
