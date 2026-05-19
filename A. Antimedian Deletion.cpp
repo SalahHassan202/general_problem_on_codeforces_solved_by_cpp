acf6b7ac4a0c88e87ae5955b026d78abc6e7f23c
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-05-19 06:02:21 +0300

    Added A. Antimedian Deletion Problem

---
+++ A. Antimedian Deletion.cpp
@@ -0,0 +1,46 @@
+// A. Antimedian Deletion
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> v(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    if (n == 1)
+    {
+        cout << 1 << endl;
+        return;
+    }
+
+    for (ll i = 0; i < n; i++)
+    {
+        cout << 2 << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    fast;
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
