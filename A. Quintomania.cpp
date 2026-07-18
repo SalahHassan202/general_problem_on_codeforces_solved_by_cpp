66077173cb7cacd2a488b00b4239da58b3d6369a
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-07-18 23:38:42 +0300

    Update A. Quintomania.cpp

--- A. Quintomania.cpp
+++ A. Quintomania.cpp
@@ -4,26 +4,21 @@
 #include <vector>
 #include <cmath>  
 using namespace std;
-
 int main()
 {
     int t;             cin >> t;
-    
     while (t--)
     {
         int size;                  cin >> size;  
         vector<int> arr(size);
-
         for (int i = 0; i <size ; i++)
         {
             cin >> arr[i];   
         }
 
         bool perfect = true;  
-
         for (int i = 1; i < size; i++)
         {
-
             if (abs(arr[i] - arr[i - 1]) != 5 && abs(arr[i] - arr[i - 1]) != 7) {
                 perfect = false ;
                 break;
