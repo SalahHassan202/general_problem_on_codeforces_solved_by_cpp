fa9a67c291ad718013ac142a044e2b2b547b9192
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-08-01 23:44:00 +0300

    Update A. Sublime Sequence.cpp

--- A. Sublime Sequence.cpp
+++ A. Sublime Sequence.cpp
@@ -9,11 +9,10 @@ int main()
     // fast code
     ios::sync_with_stdio(false),cout.tie(NULL),cin.tie(0);
     
-   ll t ; cin >>t ;
+   ll t ;                  cin >>t ;
    while(t--)
    {
-        ll x , n ; 
-        cin >> x >>n ;
+        ll x , n ;            cin >> x >>n ;
         if(n % 2 ==0)
         {
             cout << 0 << endl;
@@ -22,7 +21,7 @@ int main()
         {
             cout << x << endl;
         }
-
    }
+    
     return 0 ;
 }
