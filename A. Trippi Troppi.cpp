10a997d5e2c4b55b35a926d31287a968b46964f8
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-08-21 23:51:08 +0300

    Update A. Trippi Troppi.cpp

--- A. Trippi Troppi.cpp
+++ A. Trippi Troppi.cpp
@@ -5,9 +5,7 @@
 using namespace std ;
 void solve()
 {
-    string s1 , s2 , s3 ; 
-    cin>> s1 >>s2 >> s3 ;
-
+    string s1 , s2 , s3 ;          cin>> s1 >>s2 >> s3 ;
     cout<< s1[0] <<s2[0] <<s3[0] <<endl;
 }
 int main()
@@ -15,12 +13,10 @@ int main()
     // fast code
     ios::sync_with_stdio(false),cout.tie(NULL),cin.tie(0);
     
-   ll t ; cin >>t ;
+   ll t ;            cin >>t ;
    while(t--)
    {
-        
         solve();
-
    }
     return 0 ;
 }
