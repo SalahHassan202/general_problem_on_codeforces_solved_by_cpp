675aff7906d9030c00f57e45a7ea802279cdcb8b
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-09-12 23:39:38 +0300

    Update A. Two Vessels.cpp

--- A. Two Vessels.cpp
+++ A. Two Vessels.cpp
@@ -6,21 +6,15 @@ using namespace std ;
 int main()
 {
 
-int lines ;
-cin>>lines;
+int lines ;         cin>>lines;
 
 while (lines--)
 {
-    int a , b, c ;
-    cin >>a>>b>>c;
-
+    int a , b, c ;       cin >>a>>b>>c;
     int ans =0; 
     ans =((abs(a-b)+1)/2+c-1)/c ;
-
     cout<<ans<<endl;
 }
-
-
 
 return 0;
 }
