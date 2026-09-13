4ba3aba8be3816f855d3781a46dd6ff2492f3383
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-09-13 23:23:00 +0300

    Update A. Way Too Long Words.cpp

--- A. Way Too Long Words.cpp
+++ A. Way Too Long Words.cpp
@@ -1,19 +1,21 @@
 // A. Way Too Long Words 
+
 #include <iostream>
 using namespace std;
 int main()
 {
-int size;
-cin>> size ;
-for(int i=0; i<size ;i++){
+int size;              cin>> size ;
+for(int i=0; i<size ;i++)
+{
     string word ;             // salahhassan      11       s9n
     cin>>word;
-
     int mySize = word.length() ;
-    if(mySize >10){
+    if(mySize >10)
+    {
         cout<< word[0]<<mySize-2 <<word[mySize -1]<<endl; 
     }
-    else{
+    else
+    {
         cout << word<<endl;
     }
 }
