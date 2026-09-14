ab5dfaa33dc9e1969413b2f0d5f71ae371ec73d9
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-09-14 23:57:30 +0300

    Update A.team.cpp

--- A.team.cpp
+++ A.team.cpp
@@ -1,16 +1,13 @@
 // A.team
 
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
     int n, Petya, Vasya, Tonya, number(0);
     cin >> n;
    
     for(int i=0; i<n ; i++)
-    
     {
         cin >> Petya >> Vasya >> Tonya;
         if (Petya + Vasya + Tonya >= 2)
@@ -19,6 +16,7 @@ int main()
         }
     }
     cout << number << endl;
+    
     return 0;
 }
 
