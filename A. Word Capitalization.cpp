fc89c8777616782d07c698fbb6670db0801b0f25
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2026-09-18 23:35:45 +0300

    Update A. Word Capitalization.cpp

--- A. Word Capitalization.cpp
+++ A. Word Capitalization.cpp
@@ -1,21 +1,16 @@
 // A. Word Capitalization
+
 #include <iostream>
 #include <string>
 using namespace std;
-
 int main()
 {
-
     string word;                cin >> word;
     if (!word.empty() && islower(word[0]))
     {
         word[0] = toupper(word[0]);
     }
-
-
     cout << word << endl;
-
-
-
+    
     return 0;
 }
